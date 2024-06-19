6a8ff80f48778f1f32f5959550a26ca26935ccc3
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-19 23:13:00 +0100

    :art: style(signin): Improve layout of signin form
    - Enhance styling for better readability and user experience
    - Adjust alignment and spacing of form elements
    
    Footer: 0.0.14

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => (
-  <div>
-    <header>Login</header>
-    <main>
+  <div className="flex h-screen">
+    <aside className="w-1/2 bg-color-primary flex items-center justify-center text-4xl font-bold text-white">
+      Pipance
+    </aside>
+    <main className="w-1/2 flex flex-col items-center justify-center px-10">
       <Outlet />
     </main>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
           4: "#7ADB78",
           5: "#858DFF",
           6: "#FF98E2",
-          primary: "#287bff"
+          'primary': "#287bff"
         },
         stroke: {
           1: "#26242C",
