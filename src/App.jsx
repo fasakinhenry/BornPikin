3dd1c70ae5045cba6a572082ca43573b505e10e5
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-19 22:22:36 +0100

    :sparkles: feat(routes): Add all routes in App.jsx
    - Define application routes in App.jsx
    - Ensure all components are properly routed
    
    Footer: 0.0.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingLayout from "./layouts/LandingLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 import LandingPage from "./pages/Home";
 import Features from "./pages/Functions";
 
 const App = () => (
   <Router>
     <Routes>
+      {/* Landing page Layout */}
       <Route path="/" element={<LandingLayout />}>
         <Route index element={<LandingPage />} />
         <Route path="/functions" element={<Features />} />
+      </Route>
+      {/* Signin and Signup page layout */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+      {/* Main application or dashboard layout */}
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   </Router>
