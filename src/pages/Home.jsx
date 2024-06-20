11e10bcf63fccf812d798c96fb89c5e4c116d332
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-20 17:35:54 +0100

    :art: style(hero-section): Set up basic styling of the hero section
    - Implement initial styling for hero section
    - Ensure layout and design align with project requirements
    
    Footer: 0.0.22

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import gridLines from '../assets/grid_lines.png';
+import heroImage from '../assets/hero_image.png';
 import '../index.css';
 
 const LandingPage = () => (
   <div
-    className='relative text-center py-10 flex flex-col items-center w-full m-0 mt-5 bg-repeat h-[100vh]'
-    style={{ backgroundImage: `url(${gridLines})`, backgroundSize: 'cover' }}
+    className='relative w-full mt-5 bg-repeat min-h-screen'
+    style={{ backgroundImage: `url(${gridLines})` }}
   >
-    <div className='w-full'></div>
-    <div className=' absolute top-20 mt-10'>
-      <h1 className='text-6xl font-black mt-10'>
-        Take <span className='text-blue-600'>Care</span> of{' '}
-        <span className='text-blue-600'>Yourself</span> and{' '}
-        <span className='text-blue-600'>Pikin</span> wella
-      </h1>
-    </div>
+    <section className='relative px-10 py-12 flex flex-row justify-center items-center'>
+      <div className='md:w-1/2 mb-8 md:mb-0 flex flex-col w-full'>
+        <h1 className='text-4xl md:text-5xl font-bold mb-4'>
+          Take <span className='text-purple-600'>Care of Yourself</span>
+          <br />
+          and <span className='text-purple-600'>Pikin wella</span>
+        </h1>
+        <img
+          src={heroImage}
+          alt='Mother and child hands'
+          className='rounded-full h-90 w-90 mx-auto'
+        />
+      </div>
+      <div className='md:w-1/2'>
+        <p className='text-gray-600 mb-6'>
+          "We believe Maternal Health to be Universal Right, Not a Privilege"
+        </p>
+        <p className='text-gray-600 mb-6'>
+          The strong connection between mama and her pikin is special. We dey
+          support mama with everything wey dem need to build better families and
+          brighter futures.
+        </p>
+        <button className='bg-purple-600 text-white px-6 py-3 rounded-full hover:bg-purple-700 transition duration-300'>
+          Get Started
+        </button>
+      </div>
+    </section>
 
     {/* <div className="mt-10">
       <img
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -40,8 +40,8 @@ const LoginPage = () => {
             Account Type
           </label>
           <select className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500">
-            <option>User</option>
-            <option>Recycling company</option>
+            <option>Mother / Pregnant women</option>
+            <option>Hospitals / clinic</option>
             <option>Collector/Intermediary</option>
           </select>
         </div>
