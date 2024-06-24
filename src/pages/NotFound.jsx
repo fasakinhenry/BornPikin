678c2b14ccf8f776cb7ce5c677bbeea9286fec5f
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-24 03:11:43 +0100

    :bug: fix(notfound): Fix styling in the 404 page component
    - Correct styling issues in NotFound component
    - Ensure proper display and layout for 404 page
    
    Footer: 0.1.15

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,17 @@
-
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigateTo = useNavigate();
 
-    const navigateTo = useNavigate();
-
-    return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <h1 class="text-5xl font-bold">Error 404</h1>
-            <div class="text-5xl font-bold">Page not found</div>
-        </div>
-    );
-}
+  return (
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
+      <div className='box'>
+        <div class='text-5xl font-bold block mb-4 text-center'>Error 404</div>
+        <div class='text-2xl font-bold block text-center'>Page not found</div>
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;
